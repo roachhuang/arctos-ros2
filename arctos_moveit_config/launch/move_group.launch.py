dc56dc12c1507b44afed2df3ab024ac6f959fa1c
Mark Huang <giraftw2002@gmail.com>
2025-12-09 22:22:01 +0800

    amend: enable action server for mtc

--- arctos_moveit_config/launch/move_group.launch.py
+++ arctos_moveit_config/launch/move_group.launch.py
@@ -1,13 +1,16 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
 
-
 def generate_launch_description():
     moveit_config = (MoveItConfigsBuilder("arctos", package_name="arctos_moveit_config")
-    .planning_pipelines(pipelines=["ompl"])
-    .robot_description(file_path="config/arctos.urdf.xacro")
-    .robot_description_semantic(file_path="config/arctos.srdf")
-    .robot_description_kinematics(file_path="config/kinematics.yaml")
-    .to_moveit_configs()                        
-    )                 
+        .planning_pipelines(pipelines=["ompl"])
+        .robot_description(file_path="config/arctos.urdf.xacro")
+        .robot_description_semantic(file_path="config/arctos.srdf")
+        .robot_description_kinematics(file_path="config/kinematics.yaml")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .to_moveit_configs()                        
+        )
+    
+    moveit_config.move_group_capabilities["capabilities"] = "move_group/ExecuteTaskSolutionCapability"
+    
     return generate_move_group_launch(moveit_config)
